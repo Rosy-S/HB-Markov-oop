392c0db1fb9afd11c15cd00aab1bd2725a9c69c5
Hackbright Student <info@hackbrightacademy.com>
2015-07-10 12:25:33 -0700

    working markov-oo

--- markov.py
+++ markov.py
@@ -3,12 +3,14 @@ import random
 input_files = sys.argv[1:]
 
 
-
-class SimpleMarkovGenerator(object):
+#renaming this temporarily from SimpleMarkovGenerator to SimpleMG
+#because it's long to type
+class SimpleMG(object):
 
 
     def __init__(self, filenames):
-        self.read_files(filenames)
+        self.words = self.read_files(filenames)
+        self.dictionary = self.make_chains()
 
 
     def read_files(self, filenames):
@@ -18,7 +20,7 @@ class SimpleMarkovGenerator(object):
         for the_file in self.filenames: 
             file_contents = open(the_file).read()
             words += file_contents.split()
-        print words
+        return words
 
 
         # self.words = []
@@ -30,16 +32,42 @@ class SimpleMarkovGenerator(object):
         #     final_string += words
         # print final_string
 
-    def make_chains(self, corpus):
+    def make_chains(self):
         """Takes input text as string; stores chains."""
-        pass
 
-        # your code here
+        self.chains = {}
+        for i in range(len(self.words) - 2):
+            key = (self.words[i], self.words[i + 1])
+            value = self.words[i + 2]
 
+            if key not in self.chains:
+                self.chains[key] = []
+
+            self.chains[key].append(value)
+
+    
+
+        # or we could say "chains.setdefault(key, []).append(value)"
+        return self.chains
     def make_text(self):
         """Takes dictionary of markov chains; returns random text."""
-        pass
-        # your code here
+        key = random.choice(self.chains.keys())
+        words = [key[0], key[1]]
+        character_count = 0
+        while key in self.chains and character_count < 130:
+            # Keep looping until we have a key that isn't in the chains
+            # (which would mean it was the end of our original text)
+            #
+            # Note that for long texts (like a full book), this might mean
+            # it would run for a very long time.
+
+            word = random.choice(self.chains[key])
+            words.append(word)
+            key = (key[1], word)
+            character_count += len(word)
+
+        return " ".join(words)
+            
 
 
 if __name__ == "__main__":
